32bc6ede9e3e593050482ac7edac87dd0d4b3fad
Dave Marr <dave.marr@gmail.com>
2014-12-11 00:29:03 -0800

    tabs->spaces

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,22 @@ var server = require('../server.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	if(res.err)
+    if(res.err)
         console.error(res.err);
 
     server(null, function(err, data) {
         if (err) {
-        	console.error(err);
-        	throw err;
+            console.error(err);
+            throw err;
         }
         // data is a readable stream returned from aws-sdk
         // getObject().createReadStream
         console.log ('data from route', data.toString());
 
-		res.render('index', {
-	        title: 'S3 Bucket Data',
-	        data: data.length
-	    });
+        res.render('index', {
+            title: 'S3 Bucket Data',
+            data: data.length
+        });
      
     });
 });
--- s3-module-spike.js
+++ s3-module-spike.js
@@ -1,14 +1,14 @@
 var AWS = require('aws-sdk');
 require('es6-shim');
 module.exports = function(config) {
-	var params = Object.assign({
-		Bucket: "phormat",
-		Key: "spikes/"
-	}, config);
+    var params = Object.assign({
+        Bucket: "phormat",
+        Key: "spikes/"
+    }, config);
 
-	var s3 = new AWS.S3({
-		"logger": process.stdout
-	});
+    var s3 = new AWS.S3({
+        "logger": process.stdout
+    });
 
-	return s3.getObject(params);
+    return s3.getObject(params);
 }
