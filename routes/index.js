f67253d49134f0bd7f7c398f6cdfe4bf8573028f
Dave Marr <dave.marr@gmail.com>
2014-12-11 01:11:35 -0800

    close but no buffer size

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var server = require('../server.js');
-
+var s3object = require('../s3-module-spike');
+require('aws-sdk').config.loadFromPath('./s3config.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
     if(res.err)
         console.error(res.err);
 
-    server(null, function(err, data) {
-        if (err) {
-            console.error(err);
-            throw err;
-        }
+    var spike = new s3object(null, function(data) {
+
         // data is a readable stream returned from aws-sdk
         // getObject().createReadStream
+        console.log ('data from route', data);
         console.log ('data from route', data.toString());
 
         res.render('index', {
--- s3-module-spike.js
+++ s3-module-spike.js
@@ -1,14 +1,20 @@
 var AWS = require('aws-sdk');
 require('es6-shim');
-module.exports = function(config) {
+
+var bl = require('bl');
+
+module.exports = function(config, callback) {
     var params = Object.assign({
         Bucket: "phormat",
-        Key: "spikes/"
+        Key: "spikes/",
     }, config);
 
     var s3 = new AWS.S3({
         "logger": process.stdout
     });
 
-    return s3.getObject(params);
+    s3.getObject(params, function(err, data) {
+        if (err) throw err;
+        callback(data);
+    });
 }
