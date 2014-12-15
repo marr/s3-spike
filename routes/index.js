1ce1150af6fbe9df712033a3593c38befab1a4cd
Dave Marr <dave.marr@gmail.com>
2014-12-14 16:16:07 -0800

    start getting some data

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,29 @@ var express = require('express');
 var router = express.Router();
 var phormat = require('../s3-module-spike');
 
+var bl = require('bl');
+var parser = require('xml2json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if(res.err)
         console.error(res.err);
 
-   phormat()
+    
+    phormat()
         .createReadStream()
-        .pipe(res);
+        .pipe(bl(function(err, data) {
+            console.log(data);
+            var xml = data.toString();
+
+            data = JSON.parse(parser.toJson(xml));
+            
+            res.render('index', {
+                title: 'S3 Bucket Data',
+                data: data
+            });
+        }));
+        //.pipe(res);
 
     //next();
 });
